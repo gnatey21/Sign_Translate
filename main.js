8c4831219458ad25db4b7d4a607ce08e9dcb127f
gnatey21 <gnatey21@gmail.com>
2023-11-05 10:51:14 -0500

    First prototype
    Missing translation function

---
+++ main.js
@@ -0,0 +1,6 @@
+function getInput() {
+    let in_string = document.getElementById("string_input").value;
+    for(var i = 0; i < in_string.length; i++) {
+        let new_card = document.createElement("div");
+    }
+}
